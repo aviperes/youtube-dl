33d39eab56ec249a80dcd8a6b894499719369f21
Avichai <cavichai@videocites.com>
2020-09-27 11:08:41 +0300

    adding uploader handle (and more) to twitch vod

--- youtube_dl/extractor/twitch.py
+++ youtube_dl/extractor/twitch.py
@@ -373,17 +373,21 @@ class TwitchVodIE(TwitchBaseIE):
                     'url': thumbnail_url,
                     'preference': quality_key(thumbnail_id),
                 })
+        channel = info.get('channel',{})
         return {
             'id': info['_id'],
             'title': info.get('title') or 'Untitled Broadcast',
             'description': info.get('description'),
             'duration': int_or_none(info.get('length')),
             'thumbnails': thumbnails,
-            'uploader': info.get('channel', {}).get('display_name'),
-            'uploader_id': info.get('channel', {}).get('name'),
+            'uploader': channel.get('display_name'),
+            'uploader_id': channel.get('name'),
+            'uploader_handle': channel.get('name'),
             'timestamp': parse_iso8601(info.get('recorded_at')),
             'view_count': int_or_none(info.get('views')),
             'is_live': is_live,
+            'uploader_like_count': channel.get('followers'),
+            'uploader_url': channel.get('url')
         }
 
     def _real_extract(self, url):
